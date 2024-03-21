ec2a064dac3349a161618d8af54310e4dd8d8a30
rdarbinyan <darbinyanraf@gmail.com>
2024-03-21 12:50:14 +0400

    rename last_weight -> query_loss_weight

--- src/utils/custom_loss_functions/context_bce_with_logits.py
+++ src/utils/custom_loss_functions/context_bce_with_logits.py
@@ -7,22 +7,22 @@ class ContextBNEWithLogits(nn.Module):
     differential weighting to the last element of the input tensors.
 
     Args:
-        last_weight (float): The weight to be applied to the loss of the last element in the sequence, with
+        query_loss_weight (float): The weight to be applied to the loss of the last element in the sequence, with
             the remaining elements receiving a computed weight such that the average weight across all elements
             equals 1.
         *args: Variable length argument list for BCEWithLogitsLoss.
         **kwargs: Arbitrary keyword arguments for BCEWithLogitsLoss.
     """
-    def __init__(self, last_weight, *args, **kwargs):
+    def __init__(self, query_loss_weight, *args, **kwargs):
         super().__init__()
-        self._last_weight = last_weight
+        self._query_loss_weight = query_loss_weight
         # Initialize the BCEWithLogitsLoss with no reduction to manually compute the weighted loss
         self._bne = nn.BCEWithLogitsLoss(*args, **kwargs, reduction='none')
 
     def forward(self, predictions, targets):
         """
         Computes the weighted BCEWithLogitsLoss, where the loss for the last element in each sequence
-        is weighted by `last_weight` and the rest of the elements are weighted such that the average
+        is weighted by `query_loss_weight` and the rest of the elements are weighted such that the average
         weight across all elements is 1.
 
         Args:
@@ -36,6 +36,6 @@ class ContextBNEWithLogits(nn.Module):
         # Compute element-wise losses
         losses = self._bne(predictions, targets)
         # Compute the mean loss for all elements except the last, and separately for the last element
-        loss = (1 - self._last_weight) * losses[:, :-1].mean() + self._last_weight * losses[:, -1].mean()
+        loss = (1 - self._query_loss_weight) * losses[:, :-1].mean() + self._query_loss_weight * losses[:, -1].mean()
 
         return loss
