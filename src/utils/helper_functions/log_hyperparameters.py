5dd2abb4720b7ea618f6b28f6b47457f098c083a
Hrayr Harutyunyan <harhro@gmail.com>
2024-04-18 00:02:45 +0400

    Log new hparams

--- src/utils/helper_functions/log_hyperparameters.py
+++ src/utils/helper_functions/log_hyperparameters.py
@@ -18,16 +18,27 @@ def log_hyperparameters(
     hparams = dict()
 
     # choose which parts of hydra config will be saved to loggers
-    hparams["trainer"] = config["trainer"]
+    if "seed" in config:
+        hparams["seed"] = config["seed"]
+    hparams["spurious_setting"] = config["spurious_setting"]
+    hparams["sp_token_generation_mode"] = config["sp_token_generation_mode"]
+    hparams["v1_behavior"] = config["v1_behavior"]
     hparams["datamodule"] = config["datamodule"]
     hparams["model"] = config["model"]
     hparams["optimizer"] = config["optimizer"]
-    if "seed" in config:
-        hparams["seed"] = config["seed"]
-    if "callbacks" in config:
-        hparams["callbacks"] = config["callbacks"]
     if "scheduler" in config:
         hparams["scheduler"] = config["scheduler"]
+    hparams["trainer"] = config["trainer"]
+    if "callbacks" in config:
+        hparams["callbacks"] = config["callbacks"]
+
+    # xcloud stuff
+    if "exp_name" in config:
+        hparams["exp_name"] = config["exp_name"]
+    if "run_id" in config:
+        hparams["run_id"] = config["run_id"]
+    if "tb_exp_name" in config:
+        hparams["tb_exp_name"] = config["tb_exp_name"]
 
     # save number of model parameters
     hparams["model/params_total"] = sum(p.numel() for p in model.parameters())
