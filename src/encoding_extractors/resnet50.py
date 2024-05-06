3e3458478354adfe557135107098043397dd370c
Hrayr Harutyunyan <harhro@gmail.com>
2024-05-06 18:25:46 -0400

    Update resnet50.py
    Copied the forward pass from the torchvision code; Removed transformation_matrix

--- src/encoding_extractors/resnet50.py
+++ src/encoding_extractors/resnet50.py
@@ -6,28 +6,23 @@ from torchvision import models
 class ResNet50(nn.Module):
     def __init__(self,
                  pretrained: bool,
-                 embedding_size: int,
-                 transformation_matrix=None,
                  *args, **kwargs):
         super().__init__()
 
         self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1 if pretrained else None)
 
-        self.without_linear = nn.Sequential(*list(self.model.children())[:-1])
-
-        self.transformation_linear = None
-        if transformation_matrix is not None:
-            self.transformation_linear = nn.Linear(embedding_size, embedding_size, bias=False)
-
-            transformation_matrix = transformation_matrix.type(self.transformation_linear.weight.data.dtype)
-            self.transformation_linear.weight = nn.parameter.Parameter(transformation_matrix)
-
-        self.embedding_size = embedding_size
-
     def forward(self, x):
-        emb = self.without_linear(x).squeeze((2, 3))
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
 
-        if self.transformation_linear is not None:
-            emb = self.transformation_linear(emb)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
-        return emb
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        return x
