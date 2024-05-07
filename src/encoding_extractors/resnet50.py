549b28e6dc46b5107c77c220c8cd8fc6b583a547
Hrayr Harutyunyan <harhro@gmail.com>
2024-05-07 11:39:58 -0400

    Update resnet50.py

--- src/encoding_extractors/resnet50.py
+++ src/encoding_extractors/resnet50.py
@@ -12,17 +12,17 @@ class ResNet50(nn.Module):
         self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1 if pretrained else None)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
 
-        x = self.avgpool(x)
+        x = self.model.avgpool(x)
         x = torch.flatten(x, 1)
 
         return x
