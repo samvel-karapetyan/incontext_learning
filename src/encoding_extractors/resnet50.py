9afaa6fb46d1d52ecc61b03e875c0b0997ea4b9c
Hrayr Harutyunyan <harhro@gmail.com>
2024-05-07 18:23:52 -0400

    Update resnet50.py
    Fixed and final resnet50.

--- src/encoding_extractors/resnet50.py
+++ src/encoding_extractors/resnet50.py
@@ -1,43 +1,26 @@
-import torch.nn as nn
-
 from torchvision import models
+import torch
+import torch.nn as nn
 
 
 class ResNet50(nn.Module):
-    """
-    ResNet50 is a PyTorch Module for feature extraction using the ResNet-50 architecture.
-
-    This class wraps a pre-trained ResNet-50 model from torchvision.models, loading weights
-    trained on the ImageNet dataset. It removes the final linear layer to enable feature
-    extraction from input images.
-    """
-
     def __init__(self, *args, **kwargs):
-        """
-        Initializes the ResNet50 module.
-
-        Loads the ResNet-50 model with pre-trained weights from the ImageNet dataset
-        and creates a new module without the final linear layer for feature extraction.
-        """
         super().__init__()
 
-        # Load the ResNet-50 model with pre-trained weights
-        model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-
-        # Create a module without the final linear layer
-        self.without_linear = nn.Sequential(*list(model.children())[:-1])
+        self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
 
     def forward(self, x):
-        """
-        Performs a forward pass through the ResNet50 module.
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
 
-        Args:
-            x (torch.Tensor): The input tensor to the module, typically an image or a batch of images.
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
 
-        Returns:
-            torch.Tensor: The extracted features from the input images.
-        """
-        # Pass the input through the ResNet-50 model without the final linear layer
-        emb = self.without_linear(x).squeeze((2, 3))
+        x = self.model.avgpool(x)
+        x = torch.flatten(x, 1)
 
-        return emb
+        return x
